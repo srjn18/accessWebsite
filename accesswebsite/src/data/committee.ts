e77c37a615948aa601aa750f950998f2ab15a16c
shettyAshwitha999 <ashwithashetty321@gmail.com>
2025-12-23 18:24:41 +0530

    Create committee.ts to strore committee data

---
+++ accesswebsite/src/data/committee.ts
@@ -0,0 +1,44 @@
+export const committeeData = {
+  "2025": {
+    title: "ACCESS Committee Members of 2025-26",
+    members: [
+      {
+        name: "Shreemaa V Rai",
+        role: "President",
+        semester: "7th",
+        image: "/images/Committee Members Pics/2025-26 Members/pre.jpg"
+      },
+      {
+        name: "Raksha K",
+        role: "Vice President",
+        semester: "5th",
+        image: "/images/Committee Members Pics/2025-26 Members/vp.jpg"
+      },
+      {
+        name: "Charan R Karkera",
+        role: "Branch Captain",
+        semester: "7th",
+        image: "/images/Committee Members Pics/2025-26 Members/bc.jpg"
+      }
+      // ➕ Continue adding remaining members here
+    ]
+  },
+
+  "2024": {
+    title: "ACCESS Committee Members of 2024-25",
+    members: [
+      {
+        name: "Nidheesh N Amin",
+        role: "President",
+        semester: "8th",
+        image: "/images/Committee Members Pics/2024-25 Members/president.jpg"
+      },
+      {
+        name: "Shreemaa V Rai",
+        role: "Vice President",
+        semester: "6th",
+        image: "/images/Committee Members Pics/2024-25 Members/vp.jpg"
+      }
+    ]
+  }
+};
